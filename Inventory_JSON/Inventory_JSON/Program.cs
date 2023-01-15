2b8b0eb2893aa65ec59194dad61aa586a6f07671
Amol <ajamnik.mech@gmail.com>
2023-01-15 22:25:04 +0600

    [Amol]- JSON Inventory Data Management of Rice, Pulses and Wheats

---
+++ Inventory_JSON/Inventory_JSON/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventory_JSON;
+
+
+namespace Inventory_JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JsonInventoryMain Json = new JsonInventoryMain();
+
+            Json.Convert("C:\\AMD\\RFP244\\Object_Oriented_Programs_JSON\\Inventory_JSON\\Inventory_JSON\\Inventory.json");
+        }
+    }
+}
